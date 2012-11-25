22662d0365fcb7d723cfaec3b8d8418fb2a3a031
Lynn Root <erin.lynn.root@gmail.com>
2012-11-25 21:12:33 +0100

    fixed whitespace formatting

--- RuPyWorkshop/MyBlog/models.py
+++ RuPyWorkshop/MyBlog/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-	title = models.CharField(max_length=200)
-	body = models.TextField()
-	created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title

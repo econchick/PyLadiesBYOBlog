38f8efd2b8b588b1798c0d37e8cd987222da8ad9
Lynn Root <erin.lynn.root@gmail.com>
2012-11-25 19:01:55 +0100

    cleaning directory

--- ZieBlog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from ZieBlog.models import Post
-
-class PostAdmin(admin.ModelAdmin):
-    search_fields = ['title']
-
-admin.site.register(Post, PostAdmin)
--- ZieBlog/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Post(models.Model):
-    title = models.CharField(max_length=60)
-    body = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __unicode__(self):
-        return self.title
